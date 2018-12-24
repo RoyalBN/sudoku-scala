d1da5abab8a2b3dfb0f85ba7e8a9efa7826fffb1
RoyalBN <sebastien.samson22@gmail.com>
2018-12-24 12:09:35 +0100

    Functions answerChecker() and gameEndChecker() added

--- Sudoku.scala
+++ Sudoku.scala
@@ -2,22 +2,21 @@ import scala.io.Source
 
 object Sudoku extends App {
   
-
-  //***********************************************************************
+  // ***********************************************************************
   //                             VARIABLES
-  //***********************************************************************
+  // ***********************************************************************
   
   val sudoku = 
   Array(
-        Array(0, 0, 4, 0, 2, 8, 0, 0, 5),
-        Array(0, 3, 2, 5, 0, 9, 0, 7, 0),
-        Array(0, 5, 0, 0, 6, 0, 9, 0, 0),
-        Array(2, 7, 0, 0, 3, 1, 0, 4, 0),
-        Array(4, 6, 0, 0, 0, 0, 0, 1, 7),
-        Array(0, 1, 0, 6, 7, 0, 0, 9, 3),
-        Array(0, 0, 6, 0, 5, 0, 0, 3, 0),
-        Array(0, 4, 0, 9, 0, 6, 7, 8, 0),
-        Array(9, 0, 0, 4, 8, 0, 6, 0, 0)
+        Array(7, 9, 4, 1, 2, 8, 3, 6, 5),
+        Array(6, 3, 2, 5, 4, 9, 1, 7, 8),
+        Array(8, 5, 1, 3, 6, 7, 9, 2, 4),
+        Array(2, 7, 9, 8, 3, 1, 5, 4, 6),
+        Array(4, 6, 3, 2, 0, 5, 8, 1, 7),
+        Array(5, 1, 8, 6, 7, 4, 2, 9, 3),
+        Array(1, 8, 6, 7, 5, 2, 4, 3, 9),
+        Array(3, 4, 5, 9, 1, 6, 7, 8, 2),
+        Array(0, 2, 7, 4, 8, 3, 6, 5, 0)
   )
 
   val sudoku_solution = 
@@ -43,24 +42,29 @@ object Sudoku extends App {
   // Terminal colors settings
   val ANSI_RESET = "\u001B[0m";
   val ANSI_RED = "\u001B[31m";
+  val ANSI_GREEN = "\u001B[32m";
   val ANSI_RED_BACKGROUND = "\u001B[41m";
   val ANSI_GREEN_BACKGROUND = "\u001B[42m";
 
+  // Variable for finishing the game
+  var game_finished = 0
 
 
-  //***********************************************************************
+
+  // ***********************************************************************
   //                             FUNCTIONS
-  //***********************************************************************
+  // ***********************************************************************
   
-  // Print Sudoku grid
+  // *********************
+  //   Print Sudoku grid
+  // *********************
   def printGrid(args: Array[Array[Int]]) = {
       var i = 0; // Vertical line
       var j = 0; // Horizontal line
       var a = 0;
       var indice = 0;
 
-      // Print letters and numbers for rows and cols
-        
+
       // Print the number header
       for(indice <- 1 to 9){
          if(indice == 4 || indice == 7){
@@ -78,13 +82,11 @@ object Sudoku extends App {
             // Print the values
             print(sudoku(i)(j) + " ");
             // Print vertical lines
-            
             if(j == 2 || j == 5){
                print("| ");
             }
-            
         }
-        // Print the letters
+        // Print the letters on the side
         print(ANSI_GREEN_BACKGROUND + ANSI_RED + " " + letters(a) + " " + ANSI_RESET);
         a = a + 1;
 
@@ -100,42 +102,105 @@ object Sudoku extends App {
         }
         
       }
-      println();
+      println(); 
+  }
+  
+
+
+  // *********************
+  //   Check the answer
+  // *********************  
+  def answerChecker(args: String) {
+    var upperIndice = args.toUpperCase
+    var i1_value = 0
+    var i1 = upperIndice(0)
+    var i2 = args(2).asDigit
+    var i3 = args(4).asDigit
     
+    
+    i1 match {
+       case 'A' => i1_value = 0
+       case 'B' => i1_value = 1
+       case 'C' => i1_value = 2
+       case 'D' => i1_value = 3
+       case 'E' => i1_value = 4
+       case 'F' => i1_value = 5
+       case 'G' => i1_value = 6
+       case 'H' => i1_value = 7
+       case 'I' => i1_value = 8
+       case _ => println("Letter needed for argument 1")
+    }
+
+
+    // If correct : Print the number
+    if(i3 == sudoku_solution(i1_value)(i2-1)){
+       sudoku(i1_value)(i2-1) = i3
+       println("Correct answer !")
+       println("Grid updated")
+       gameEndChecker(sudoku)
+    }
+    else println("Wrong answer, try again...")  
+
+    
+
   }
+
+
+  // *********************
+  //  Check for Game End
+  // ********************* 
+  def gameEndChecker(args: Array[Array[Int]]){
+    // Check if the game is finished
+    var i = 0
+    var j = 0
+    game_finished = 1
+    for(i <- 0 to 8){ 
+        for(j <- 0 to 8){
+            if(sudoku(i)(j) == 0){
+               game_finished = 0 
+            }
+            
+        }  
+    }
+
+  }
+
   
-  /*
-  def rowChecker(row: Array[Array[Int]]) = {
-    // For loop: Check if a row only have different numbers
-  }
 
-  def colChecker(col: Array[Array[Int]]) = {
-    // For loop: Check if a col only have different numbers
-  }
-  */
-
-
-  def zeroChecker(args: Array[Array[Int]]){
-    // For loop: Check there is/are zero
-  }
-
-
-
-
-  //***********************************************************************
-  //                               MAIN
-  //***********************************************************************
+  // ***********************************************************************
+  //                                MAIN
+  // ***********************************************************************
   
-  // val name = scala.io.StdIn.readLine("What's your name? ")
-  // println("Hello " + name)
+  // Print player's name
+  val name = scala.io.StdIn.readLine("What's your name? ")
+  println("Hello " + name)
 
   // Print the rules for playing the game  
   for (line <- Source.fromFile(filename).getLines) {
-    println(line)
+     println(line)
   }
 
 
-  printGrid(sudoku)
+  // Main loop 
+  while(game_finished != 1){
 
+    // Print the Sudoku grid
+    printGrid(sudoku)
+       
+    // User entries
+    var choice = scala.io.StdIn.readLine("Choose a position and a number:")
+    val issues = choice.length() != 5 || choice(1) != ','|| choice(3) != ':' ||
+                 choice(2).asDigit < 1 || choice(2).asDigit > 9 ||
+                 choice(4).asDigit < 1 || choice(4).asDigit > 9 || choice == "";
   
+    // Check if there is a issue
+    if(issues){
+       println("Something wrong with the syntax...Try again")
+    }
+    else answerChecker(choice)  
+   
+  }
+  printGrid(sudoku)
+  println("Congratulation " + name + " ! You solved the Sudoku !")
+
 }
